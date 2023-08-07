31b07adf0f2e5f438e3d0efbd71ff5fe5dec3e92
Hardik <hrsanghani157@gmail.com>
2023-08-07 18:16:32 +0530

    - Initial Committee

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    distDir: "build",
+    images: {
+        unoptimized: true,
+        // domains: "http://103.232.124.169:20019",
+      },
+    };
+
+module.exports = nextConfig
+
+
+// const nextConfig = {
+//   /* config options here */
+// }
+ 
+// module.exports = nextConfig
---
+++ src/components/carousal.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ResponsiveCarousal = ({children,arrow,dots,slidetoshow}:any) => {
+  const settings = {
+    dots: dots,
+    infinite: true,
+    arrows: arrow,
+    speed: 500,
+    autoplay: false,
+    slidesToShow: slidetoshow,
+    slidesToScroll: 1,
+    centerMode:false,
+    responsive: [
+      
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  }
+  return (
+    <div>
+       <Slider {...settings}>
+        {children}
+          {/* <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div> */}
+        </Slider>
+    </div>
+  )
+}
+
+export default ResponsiveCarousal
